9a9cadc81be788394aaa3285a454f48ffc0b3f25
Greg Liras <gregliras@gmail.com>
2011-02-24 13:56:13 +0200

    added pyc files

--- time_mod.py
+++
@@ -1,5 +0,0 @@
-from time import strftime
-from colors import set_colors,set_normal_color
-from config_mod import TIME_COLOR,TIME_BACKGROUND_COLOR
-def NOW():
-  return " "+set_colors(TIME_COLOR,TIME_BACKGROUND_COLOR)+strftime("%d-%m %H:%M")+set_normal_color()
