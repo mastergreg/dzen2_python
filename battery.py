c6798e79dcaa166dd285c5e04dc55a5ad02295e4
Greg Liras <gregliras@gmail.com>
2011-03-02 19:00:11 +0200

    added battery color

--- battery.py
+++ battery.py
@@ -1,5 +1,7 @@
 from threading import Thread
 from time import sleep
+from subprocess import Popen,PIPE
+from colors import set_measure_color,set_normal_color
 BATTERY=""
 class get_battery(Thread):
   def run(self):
@@ -9,7 +11,21 @@ class get_battery(Thread):
       f2 = open('/sys/class/power_supply/CMB1/charge_now','r')
       full=float(f1.readline())
       current=float(f2.readline())
-      BATTERY=" ^i(/home/master/.icons/dzen2/power-bat.xbm)"+str(int(current*100/full))+"%"
+      percent=int(current*100/full)
+#      if percent<90:
+#        battery_notification().start()
+      BATTERY=" ^i(/home/master/.icons/dzen2/power-bat.xbm)"+set_measure_color(100-percent)+str(percent)+set_normal_color()+"%"
       f1.close()
       f2.close()
       sleep(60)
+class battery_notification(Thread):
+  def run(self):
+    while True:
+      notif=Popen("dzen2 -fn '-misc-fixed-bold-r-semicondensed--13-90-100-100-c-60-iso8859-1' -w 150 -x 400 -y 300 -bg black -fg red",shell=True,stdin=PIPE)
+      notif.stdin.write("LOW BATTERY\n")
+      sleep(1)
+      notif.kill()
+      notif.wait()
+      print notif.returncode
+      sleep(1)
+
