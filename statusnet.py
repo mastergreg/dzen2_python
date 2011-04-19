adfb13ea7162e5790405097916056cd20a7c697c
Greg Liras <gregliras@gmail.com>
2011-04-19 23:20:25 +0300

    rotation of statusnet

--- statusnet.py
+++ statusnet.py
@@ -8,24 +8,24 @@ from colors import *
 STATUSNET=""
 
 class get_statusnet(threading.Thread):
-	def run( self ):
-		global STATUSNET
+  def run( self ):
+    global STATUSNET
 
-		while True:
-			try:
-				status = simplejson.load(urllib2.urlopen("http://status.foss.ntua.gr/index.php/api/statuses/public_timeline.json?count=3"))
-	#			status = simplejson.load(urllib2.urlopen(STATUSNET_URL))
+    while True:
+      try:
+        status = simplejson.load(urllib2.urlopen("http://status.foss.ntua.gr/index.php/api/statuses/public_timeline.json?count=3"))
+    #			status = simplejson.load(urllib2.urlopen(STATUSNET_URL))
 
-	#			STATUSNET = status[1]['user']['name']
-				for i in range(0,2):
-					status_user = ' '+set_colors(STATUSNET_USER_COLOR,STATUSNET_BACKGROUND_COLOR)+status[i]['user']['name']
-					text = status[i]['text']
-					for j in range(0,len(text)):
-						status_text = ' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+text[i:]
-						STATUSNET = status_user + status_text
-					sleep(5)
-				continue		
-			except (urllib2.URLError):
-				STATUSNET=' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+"Cannot connect"+set_normal_color()
-				sleep(60)
-				
+        #			STATUSNET = status[1]['user']['name']
+        for i in range(0,2):
+          status_user = ' '+set_colors(STATUSNET_USER_COLOR,STATUSNET_BACKGROUND_COLOR)+status[i]['user']['name']
+          text = status[i]['text']
+          for j in range(0,len(text)-15,20):
+            status_text = ' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+text[j:j+15]
+            STATUSNET =  status_user + status_text
+            sleep(5)
+            continue		
+      except (urllib2.URLError):
+        STATUSNET=' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+"Cannot connect"+set_normal_color()
+        sleep(60)
+
