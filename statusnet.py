e2f989cf217f0c43b01a2f1171e719b216f2c208
alex <alzaxo@gmai.com>
2011-04-19 22:32:10 +0300

    status

--- statusnet.py
+++ statusnet.py
@@ -13,14 +13,17 @@ class get_statusnet(threading.Thread):
 
 		while True:
 			try:
-				status = simplejson.load(urllib2.urlopen(STATUSNET_URL))
+				status = simplejson.load(urllib2.urlopen("http://status.foss.ntua.gr/index.php/api/statuses/public_timeline.json?count=3"))
+	#			status = simplejson.load(urllib2.urlopen(STATUSNET_URL))
+
+	#			STATUSNET = status[1]['user']['name']
 				for i in range(0,2):
-					status_user = ' '+set_colors(STATUSNET_USER_COLOR,STATUSNET_BACKGROUND_COLOR)+status[i]['user']
+					status_user = ' '+set_colors(STATUSNET_USER_COLOR,STATUSNET_BACKGROUND_COLOR)+status[i]['user']['name']
 					text = status[i]['text']
 					for j in range(0,len(text)):
 						status_text = ' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+text[i:]
 						STATUSNET = status_user + status_text
-						sleep(100)
+					sleep(1)
 				continue		
 			except (urllib2.URLError):
 				STATUSNET=' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+"Cannot connect"+set_normal_color()
