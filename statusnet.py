9c8d82f5f0eed5ce2b456195d206f7c5a51e6328
alex <alzaxo@gmai.com>
2011-04-20 17:11:47 +0300

    greeklish on statusnet

--- statusnet.py
+++ statusnet.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-s
 from time import sleep
 import simplejson
 import threading
@@ -5,7 +6,8 @@ import re
 import urllib2
 from config_mod import STATUSNET_URL,STATUSNET_COLOR,STATUSNET_USER_COLOR, STATUSNET_BACKGROUND_COLOR
 from colors import *
-STATUSNET=""
+import string
+STATUSNET=" "
 
 class get_statusnet(threading.Thread):
   def run( self ):
@@ -17,13 +19,23 @@ class get_statusnet(threading.Thread):
     #			status = simplejson.load(urllib2.urlopen(STATUSNET_URL))
 
         #			STATUSNET = status[1]['user']['name']
-        for i in range(0,2):
-          status_user = ' '+set_colors(STATUSNET_USER_COLOR,STATUSNET_BACKGROUND_COLOR)+status[i]['user']['name']
+        for i in range(0,3):
+          status_user = ' '+set_colors(STATUSNET_USER_COLOR,STATUSNET_BACKGROUND_COLOR)+'StatusNet [' + str(i+1) + '/3]: ' +status[i]['user']['name']
           text = status[i]['text']
-          for j in range(0,len(text)-15,20):
-            status_text = ' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+text[j:j+15]
+          text = text.decode('utf-8')
+          text = text.encode('iso-8859-7', 'replace')
+          
+	  from_chars = u'áâãäåæçèéêëìíîïðñóòôõö÷øùÜÝÞßúÀüýûàþÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÓÔÕÖ×ØÙ¶¸¹ºÚ¼¾Û¿'.encode('latin1')
+	  to_chars =  u'abgdezh8iklmn3oprsstufxywaehiiiouuuwABGDEZH8IKLMNJOPRSTYFXCWAEHIIOUUW'.encode('latin1')
+	  trantab = string.maketrans( from_chars, to_chars )
+	  #trantab = dict((ord(a), b) for a, b in zip(from_chars, to_chars))
+	  text = string.translate( text, trantab )
+	  #text = text.translate(trantab)
+	  #text = test
+	  for j in range(0,len(text),20):
+            status_text = ' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+text[j:j+25]
             STATUSNET =  status_user + status_text
-            sleep(5)
+            sleep(2)
             continue		
       except (urllib2.URLError):
         STATUSNET=' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+"Cannot connect"+set_normal_color()
