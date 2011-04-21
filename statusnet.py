f08fc8e5c1892321554d51573c8e8c9b8d3bf16e
Greg Liras <gregliras@gmail.com>
2011-04-21 14:25:45 +0300

    seperated thread for downloading url and prosessing json object in statusnet

--- statusnet.py
+++ statusnet.py
@@ -1,26 +1,36 @@
 # -*- coding: utf-8 -*-s
 from time import sleep
 import simplejson
-import threading
+from threading import Thread
 import re
 import urllib2
 from config_mod import STATUSNET_URL,STATUSNET_COLOR,STATUSNET_USER_COLOR, STATUSNET_BACKGROUND_COLOR
 from colors import *
 import string
 STATUSNET=" "
-
-class get_statusnet(threading.Thread):
-  def run( self ):
-    global STATUSNET
-
+STATUS=""
+class download_url(Thread):
+  def run ( self ):
+    global STATUS,STATUSNET
     while True:
       try:
-        status = simplejson.load(urllib2.urlopen("http://status.foss.ntua.gr/index.php/api/statuses/public_timeline.json?count=3"))
+        STATUS = simplejson.load(urllib2.urlopen("http://status.foss.ntua.gr/index.php/api/statuses/public_timeline.json?count=3"))
+        sleep(60)
+      except (urllib2.URLError):
+        STATUSNET=' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+"Cannot connect"+set_normal_color()
+        sleep(60)
+
+class get_statusnet(Thread):
+  def run( self ):
+    global STATUS,STATUSNET
+    download_url().start()
+    while True:
+      try:
         #			status = simplejson.load(urllib2.urlopen(STATUSNET_URL))
         #			STATUSNET = status[1]['user']['name']
         for i in range(0,3):
-          status_user = ' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+'StatusNet [' + str(i+1) + '/3]: ' +set_colors(STATUSNET_USER_COLOR,STATUSNET_BACKGROUND_COLOR)+status[i]['user']['screen_name']
-          text = status[i]['text']
+          status_user = ' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+'StatusNet [' + str(i+1) + '/3]: ' +set_colors(STATUSNET_USER_COLOR,STATUSNET_BACKGROUND_COLOR)+STATUS[i]['user']['screen_name']
+          text = STATUS[i]['text']
           text = text.decode('utf-8')
           text = text.encode('iso-8859-7', 'replace')
 
@@ -35,8 +45,5 @@ class get_statusnet(threading.Thread):
             status_text = ' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+text[j:j+25]
             STATUSNET =  status_user + status_text
             sleep(3)
-            continue		
-      except (urllib2.URLError):
-        STATUSNET=' '+set_colors(STATUSNET_COLOR,STATUSNET_BACKGROUND_COLOR)+"Cannot connect"+set_normal_color()
-        sleep(60)
-
+      except:
+        sleep(3)
