17fc84b72f6341550a9700f5034404ac60e1e266
Greg Liras <gregliras@gmail.com>
2011-04-22 21:31:03 +0300

    fixed a bug in gmail_check

--- gmail_check.py
+++ gmail_check.py
@@ -7,12 +7,12 @@ UNREAD=" NoConnection"
 class gmail(Thread):
   def run(self):
     global UNREAD
-    srv=srv = imaplib.IMAP4_SSL("imap.gmail.com")
-    count=[]
-    srv.login(GMAIL_USERNAME,GMAIL_PASSWORD)
-    srv.select()
     while True:
       try:
+        srv = imaplib.IMAP4_SSL("imap.gmail.com")
+        count=[]
+        srv.login(GMAIL_USERNAME,GMAIL_PASSWORD)
+        srv.select()
         status,count=srv.search(None,'UnSeen')
       #unseen= len(c.get_inbox_conversations(is_unread=True))
         UNREAD=" "+set_colors(GMAIL_UNREAD_COLOR,GMAIL_BACKGROUND_COLOR)+str(len(count[0][1:].split()))+" "+set_colors(GMAIL_COLOR,GMAIL_BACKGROUND_COLOR)+"Emails"+set_normal_color()
