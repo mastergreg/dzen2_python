2ac6bd8c609ec3686599cb0555afdf00079d9ca8
Greg Liras <gregliras@gmail.com>
2011-05-02 17:24:11 +0300

    mail icon

--- gmail_check.py
+++ gmail_check.py
@@ -1,6 +1,6 @@
 from time import sleep
 from threading import Thread
-from config_mod import GMAIL_BACKGROUND_COLOR,GMAIL_COLOR,GMAIL_UNREAD_COLOR,GMAIL_USERNAME,GMAIL_PASSWORD
+from config_mod import GMAIL_BACKGROUND_COLOR,GMAIL_COLOR,GMAIL_UNREAD_COLOR,GMAIL_USERNAME,GMAIL_PASSWORD,ICON_PATH
 from colors import *
 import imaplib
 UNREAD=" NoConnection"
@@ -15,7 +15,7 @@ class gmail(Thread):
         srv.select()
         status,count=srv.search(None,'UnSeen')
       #unseen= len(c.get_inbox_conversations(is_unread=True))
-        UNREAD=" "+set_colors(GMAIL_UNREAD_COLOR,GMAIL_BACKGROUND_COLOR)+str(len(count[0][1:].split()))+" "+set_colors(GMAIL_COLOR,GMAIL_BACKGROUND_COLOR)+"Emails"+set_normal_color()
+        UNREAD=" "+set_colors(GMAIL_UNREAD_COLOR,GMAIL_BACKGROUND_COLOR)+str(len(count[0][1:].split()))+set_colors(GMAIL_COLOR,GMAIL_BACKGROUND_COLOR)+" ^i("+ICON_PATH+"/envelope.xbm)"+set_normal_color()
         srv.close()
         srv.logout()
         sleep(60)
