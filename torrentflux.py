049b224f736ec70161c5ee4265651b9632201b24
Greg Liras <gregliras@gmail.com>
2011-05-04 14:08:03 +0300

    fixed bug in torrentflux

--- torrentflux.py
+++ torrentflux.py
@@ -19,13 +19,16 @@ class get_tflux(Thread):
     opener.open('http://'+address+'/login.php', login_data)
     #print resp.readlines()
     while 1:
-      resp = opener.open('http://'+address+'/index.php?iid=servermon')
+      resp = opener.open('http://'+address+'/index.php?iid=index')
       mytext=''.join(BeautifulSoup.BeautifulSoup(resp).findAll(text=True))
       mytext=mytext.split()
       for i in range(len(mytext)):
         if mytext[i]=="Download":
-          D_SPEED=mytext[i+3]+mytext[i+4][0]
-        if mytext[i]=="Upload":
-          U_SPEED=mytext[i+3]+mytext[i+4][0]
+          D_SPEED=mytext[i+2]+mytext[i+3][0]
+          break
+      for i in range(len(mytext)):
+        if mytext[i]=="Upload" and mytext[i+1]=='Speed:':
+          U_SPEED=mytext[i+2]+mytext[i+3][0]
+          break
       SPEEDS=' D '+D_SPEED+' U '+U_SPEED
       sleep(1)
