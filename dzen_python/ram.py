2b3752012703f60fc01cd07969e36ba4e4858342
Gregory Liras <gregliras@gmail.com>
2011-12-31 17:51:47 +0200

    fixed ram usage, no psutil for ram please

--- dzen_python/ram.py
+++ dzen_python/ram.py
@@ -1,23 +1,25 @@
 from threading import Thread
-from psutil import used_phymem,TOTAL_PHYMEM
 from config_mod import RAM_SLEEP,ICON_PATH 
 from time import sleep
 from colors import set_normal_color, set_measure_color, set_gradient_color
 
-RAM=""
-
-def ram():
-  return RAM
-class get_ram(Thread):
-  def run(self):
-    global RAM,RAM_SLEEP
-    while 1:
-      used=bytes_to_mb(used_phymem()-cached_mem()-buffers())
-      percentage=(100*used)/TOTAL_RAM_MB
-      RAM="^i("+ICON_PATH+"/mem.xbm) "+set_gradient_color(percentage)+str(used)+set_normal_color()+"M"
-      sleep(int(RAM_SLEEP))
-def bytes_to_mb(byte):
-  return byte/1048576
+def free_mem():
+  """Return the free amount  memory on the system, in bytes."""
+  f = open('/proc/meminfo', 'r')
+  for line in f:
+    if line.startswith('MemFree:'):
+      f.close()
+      return int(line.split()[1]) * 1024
+def total_mem():
+  """Return the total amount of memory on the system, in bytes."""
+  f = open('/proc/meminfo', 'r')
+  for line in f:
+    if line.startswith('MemTotal:'):
+      f.close()
+      return int(line.split()[1]) * 1024
+def used_phymem():
+  """Return the total amount of memory on the system, in bytes."""
+  return total_mem()-free_mem()
 def cached_mem():
   """Return the amount of cached memory on the system, in bytes."""
   f = open('/proc/meminfo', 'r')
@@ -39,5 +41,20 @@ def buffers():
     if line.startswith('Buffers:'):
       f.close()
       return int(line.split()[1]) * 1024
-TOTAL_RAM=TOTAL_PHYMEM
+
+def bytes_to_mb(byte):
+  return byte/1048576
+RAM=""
+TOTAL_RAM=total_mem()
 TOTAL_RAM_MB=bytes_to_mb(TOTAL_RAM)
+
+def ram():
+  return RAM
+class get_ram(Thread):
+  def run(self):
+    global RAM,RAM_SLEEP
+    while 1:
+      used=bytes_to_mb(used_phymem()-cached_mem()-buffers())
+      percentage=(100*used)/TOTAL_RAM_MB
+      RAM="^i("+ICON_PATH+"/mem.xbm) "+set_gradient_color(percentage)+str(used)+set_normal_color()+"M"
+      sleep(int(RAM_SLEEP))
